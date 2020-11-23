c75d0eb1eeded75add0f8a6b71bced59f2148ec4
Minggl <guangliang1120@gmail.com>
2020-11-23 15:08:51 +0800

    MyBatis 主配置文件详解

--- src/main/java/com/github/mgljava/dao/PeopleDao.java
+++ src/main/java/com/github/mgljava/dao/PeopleDao.java
@@ -2,8 +2,10 @@ package com.github.mgljava.dao;
 
 import com.github.mgljava.domain.People;
 import java.util.List;
+import org.apache.ibatis.annotations.Select;
 
 public interface PeopleDao {
 
+  @Select("SELECT * FROM people")
   List<People> findAll();
 }
